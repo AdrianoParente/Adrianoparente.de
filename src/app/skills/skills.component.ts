7e65a6bfc6b7ccd0dbdab93dbcd6501eb331dfa0
Adriano Parente <adriano.parente@gmx.de>
2023-11-10 17:20:52 +0100

    changed email address and skill circle load method

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
+export class SkillsComponent implements OnInit, AfterViewInit {
+  skillNameArray1: string[] = ['M', 'y'];
+  skillNameArray2: string[] = ['S', 'k', 'i', 'l', 'l', 's'];
 
-export class SkillsComponent implements OnInit {
-  skillNameArray1:string[] = ['M','y'];
-  skillNameArray2:string[] = ['S','k','i','l','l','s'];  
-
-  constructor() { }
-
-  ngOnInit(): void {    
+  constructor() {}
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      const script = document.createElement('script');
+      script.src = '../../script.js';
+      document.body.appendChild(script);
+    }, 500);
   }
 
-  
+  ngOnInit(): void {}
 }
